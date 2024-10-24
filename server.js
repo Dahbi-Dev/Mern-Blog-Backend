1c1a98008e9e47e2280c9c57c9ae1e84cda8216b
Dahbi-Dev <houssamdahbi007@gmail.com>
2024-10-25 00:47:55 +0100

    update links

--- server.js
+++ server.js
@@ -1,40 +1,42 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const User = require('./models/user');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-const multer = require('multer');
-const uploadMiddleware = multer({ dest: 'uploads/' });
-const fs = require('fs');
+const User = require("./models/user");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const uploadMiddleware = multer({ dest: "uploads/" });
+const fs = require("fs");
+require("dotenv").config();
 
-app.use('/uploads', express.static(__dirname + '/uploads'));
+app.use("/uploads", express.static(__dirname + "/uploads"));
 
+const Post = require("./models/post");
 
-const Post = require('./models/post');
-app.options('*', cors(corsOptions));
-
+const url = process.env.URL
+const secret = process.env.SECRET;
+const username = process.env.USER;
+const password = process.env.PASS;
 // app.use(cors({ credentials: true, origin: 'https://blog-mern-xi.vercel.app' }));
 const corsOptions = {
   credentials: true,
-  origin: 'https://mern-blog-frontend-fawn.vercel.app',
+  origin: url,
 };
 
+app.options("*", cors(corsOptions));
 app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
 
-const secret = "34kdjflksjfsldjf34lkjkdl";
 
-  
-  mongoose.connect("mongodb+srv://hossamdahbi626:eH8o0UNp6hUQrnyY@cluster0.r1s0spf.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@cluster0.7no7s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+);
 
-
-
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   const { username, password } = req.body;
   const salt = bcrypt.genSaltSync(10);
   const hashedPassword = bcrypt.hashSync(password, salt);
@@ -42,16 +44,16 @@ app.post('/register', async (req, res) => {
     const UserDoc = await User.create({ username, password: hashedPassword });
     res.json(UserDoc);
   } catch (e) {
-    handleError(res, e, 'An error occurred while registering a user');
+    handleError(res, e, "An error occurred while registering a user");
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   try {
     const userDoc = await User.findOne({ username });
     if (!userDoc) {
-      res.status(400).json('User not found');
+      res.status(400).json("User not found");
       return;
     }
     const passOK = bcrypt.compareSync(password, userDoc.password);
@@ -59,68 +61,65 @@ app.post('/login', async (req, res) => {
     if (passOK) {
       jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
         if (err) {
-          handleError(res, err, 'An error occurred while generating a token');
+          handleError(res, err, "An error occurred while generating a token");
         } else {
-          res.cookie('token', token).json({
+          res.cookie("token", token).json({
             id: userDoc._id,
             username,
           });
         }
       });
     } else {
-      res.status(400).json('Wrong credentials');
+      res.status(400).json("Wrong credentials");
     }
   } catch (error) {
-    handleError(res, error, 'An error occurred while logging in');
+    handleError(res, error, "An error occurred while logging in");
   }
 });
 
-app.get('/profile', (req, res) => {
+app.get("/profile", (req, res) => {
   const { token } = req.cookies;
   jwt.verify(token, secret, {}, (err, info) => {
     if (err) {
-      handleError(res, err, 'An error occurred while verifying the token');
+      handleError(res, err, "An error occurred while verifying the token");
     } else {
       res.json(info);
     }
   });
 });
 
-app.post('/logout', (req, res) => {
-  res.cookie('token', '').json('ok');
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json("ok");
 });
 
-const UserModel = require('./models/user');
+const UserModel = require("./models/user");
 
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
     const users = await UserModel.find();
     res.json(users);
   } catch (error) {
-    handleError(res, error, 'An error occurred while fetching users');
+    handleError(res, error, "An error occurred while fetching users");
   }
 });
 
-
-
-
-app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   try {
     const { originalname, path } = req.file;
-    const parts = originalname.split('.');
+    const parts = originalname.split(".");
     const ext = parts[parts.length - 1];
-    const newPath = path + '.' + ext;
+    const newPath = path + "." + ext;
     fs.renameSync(path, newPath);
 
     const { token } = req.cookies;
     jwt.verify(token, secret, {}, async (err, info) => {
       if (err) {
-        handleError(res, err, 'Authentication error');
+        handleError(res, err, "Authentication error");
       } else {
         const { title, summary, content } = req.body;
 
         // Check if the user is Houssam (adjust the condition as needed)
-        if (info.username === 'Houssam') {
+        if (info.username === "Houssam") {
           try {
             const postDoc = await Post.create({
               title,
@@ -131,46 +130,53 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
             });
             res.json({ postDoc });
           } catch (createError) {
-            handleError(res, createError, 'An error occurred while creating the post');
+            handleError(
+              res,
+              createError,
+              "An error occurred while creating the post"
+            );
           }
         } else {
-          res.status(403).json('Permission denied. Only Houssam can create a post.');
+          res
+            .status(403)
+            .json("Permission denied. Only Houssam can create a post.");
         }
       }
     });
   } catch (fileError) {
-    handleError(res, fileError, 'An error occurred while processing the file');
+    handleError(res, fileError, "An error occurred while processing the file");
   }
 });
 
-app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+app.put("/post", uploadMiddleware.single("file"), async (req, res) => {
   try {
     let newPath = null;
     if (req.file) {
       const { originalname, path } = req.file;
-      const parts = originalname.split('.');
+      const parts = originalname.split(".");
       const ext = parts[parts.length - 1];
-      newPath = path + '.' + ext;
+      newPath = path + "." + ext;
       fs.renameSync(path, newPath);
     }
 
     const { token } = req.cookies;
     jwt.verify(token, secret, {}, async (err, info) => {
       if (err) {
-        handleError(res, err, 'Authentication error');
+        handleError(res, err, "Authentication error");
       } else {
         const { id, title, summary, content } = req.body;
         const postDoc = await Post.findById(id);
 
         if (!postDoc) {
-          res.status(404).json({ error: 'Post not found' });
+          res.status(404).json({ error: "Post not found" });
           return;
         }
 
-        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+        const isAuthor =
+          JSON.stringify(postDoc.author) === JSON.stringify(info.id);
 
         if (!isAuthor) {
-          res.status(403).json('You are not the author');
+          res.status(403).json("You are not the author");
           return;
         }
 
@@ -187,24 +193,27 @@ app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
           );
           res.json(updatedPost);
         } catch (updateError) {
-          handleError(res, updateError, 'An error occurred while updating the post');
+          handleError(
+            res,
+            updateError,
+            "An error occurred while updating the post"
+          );
         }
       }
     });
   } catch (fileError) {
-    handleError(res, fileError, 'An error occurred while processing the file');
+    handleError(res, fileError, "An error occurred while processing the file");
   }
 });
 
-
-app.delete('/post/:id', async (req, res) => {
+app.delete("/post/:id", async (req, res) => {
   const postId = req.params.id;
 
   // Verify the user's token
   const { token } = req.cookies;
   jwt.verify(token, secret, {}, async (err, userInfo) => {
     if (err) {
-      return handleError(res, err, 'Authentication error');
+      return handleError(res, err, "Authentication error");
     }
 
     try {
@@ -212,53 +221,52 @@ app.delete('/post/:id', async (req, res) => {
       const postDoc = await Post.findByIdAndDelete(postId);
 
       if (!postDoc) {
-        return res.status(404).json({ error: 'Post not found' });
+        return res.status(404).json({ error: "Post not found" });
       }
 
       if (postDoc.author.toString() !== userInfo.id) {
-        return res.status(403).json('Permission denied. You are not the author of this post.');
+        return res
+          .status(403)
+          .json("Permission denied. You are not the author of this post.");
       }
 
-      return res.json({ message: 'Post deleted successfully' });
+      return res.json({ message: "Post deleted successfully" });
     } catch (error) {
-      return handleError(res, error, 'An error occurred while deleting the post');
+      return handleError(
+        res,
+        error,
+        "An error occurred while deleting the post"
+      );
     }
   });
 });
 
-
-
-app.get('/post', async (req, res) => {
+app.get("/post", async (req, res) => {
   try {
     const posts = await Post.find()
-      .populate('author', ['username'])
+      .populate("author", ["username"])
       .sort({ createdAt: -1 })
       .limit(20);
     res.json(posts);
   } catch (error) {
-    handleError(res, error, 'An error occurred while fetching posts');
+    handleError(res, error, "An error occurred while fetching posts");
   }
 });
 
-app.get('/post/:id', async (req, res) => {
+app.get("/post/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const postDoc = await Post.findById(id).populate('author', ['username']);
+    const postDoc = await Post.findById(id).populate("author", ["username"]);
     if (!postDoc) {
-      res.status(404).json({ error: 'Post not found' });
+      res.status(404).json({ error: "Post not found" });
     } else {
       res.json(postDoc);
     }
   } catch (error) {
-    handleError(res, error, 'An error occurred while fetching the post');
+    handleError(res, error, "An error occurred while fetching the post");
   }
 });
 
-
-
-
-
 app.listen(3001, () => {
-  console.log('Server Running On Port 3001!');
+  console.log("Server Running On Port 3001!");
 });
-
